f5991275bc677d831df700ec942c166cbd856388
mithindev <mithindev1@gmail.com>
2022-11-19 07:21:10 +0530

    updated

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,13 @@
+console.log("hello, World!");
+console.log("Test");
+console.log("Welcome to variables!");
+
+var variableName = "Mithin Dev";
+console.log(variableName);
+
+let a = "mithin";
+console.log(a);
+
+const b = "dev";
+console.log(b);
+
