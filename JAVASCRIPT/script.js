c02d2ac14a2114741b1f010f0ca645b0f2abee58
mithindev <mithindev1@gmail.com>
2022-11-21 08:48:49 +0530

    updated

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -11,3 +11,56 @@ console.log(a);
 const b = "dev";
 console.log(b);
 
+// this is a Single line comments
+
+/* 
+
+THis 
+is 
+a multiline 
+comment
+
+*/
+
+// Creating a variable using var keyword
+
+var c = 'mithin'; 
+
+console.log(c);
+
+// comments are not intrepeted by the javascript engine
+
+// VARIABLE NAMING RULES:-
+// 1. NAME SHOULD B UNIQUE
+// 2. NO RESERVED KEYWORD
+// 3. NAME CAN START ONLY WITH CHARACTER,&,_
+
+// String
+// three types
+
+const singleQuotes = 'Hello!'; //SIMPLE STRING
+const doubleQuotes = "Hello!!"; // SIMPLE STRING
+const backtics = `Hello!!!`; // COMPLEX STRING
+
+console.log(singleQuotes);
+console.log(doubleQuotes);
+console.log(backtics);
+
+const e = `${34 + 34}`;
+console.log(e);
+
+const wholeNumber = 23;
+console.log(wholeNumber);
+
+const dec = 23.4343;
+console.log(dec);
+
+const a1 = 10;
+const a2 = 100;
+
+const result = a1 + a2;
+console.log(result);
+
+// loose equlity disent compare data types
+
+
