f61b1e688f0eab8881b5004844b0beb276a47d93
mithindev <mithindev1@gmail.com>
2022-12-02 08:47:25 +0530

    updated in react

---
+++ REACT-JS/sanity-yt-build/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        <Link href="/">
+        
+        </Link>
+      </div>
+
+      <div></div>
+    </header>
+  )
+}
+
+export default Header
