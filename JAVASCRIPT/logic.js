4aed597dffec36f063b4c106285bbd4c286a164f
mithindev <mithindev1@gmail.com>
2022-12-02 06:36:34 +0530

    updated react js

---
+++ JAVASCRIPT/logic.js
@@ -0,0 +1,18 @@
+// const age = 18;
+
+// if (age > 18) {
+//   console.log("You may enter!");
+// } else {
+//   console.log("You may not enter!");
+// }
+
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+
+i = 0; // An eternal variable;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
